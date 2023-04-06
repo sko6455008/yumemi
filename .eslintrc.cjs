ae4b05b03c8e240abe194329bd5871dae4719583
sasaki-kodai <sasaki-kodai@aevic.co.jp>
2023-04-06 22:53:02 +0900

    chore:resolving EsLint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
     "prettier",
   ],
   rules: {
+    "react/react-in-jsx-scope": "off",
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
   },
--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,20 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          type="button"
+          onClick={() => setCount((precount) => precount + 1)}
+        >
+          {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code>
