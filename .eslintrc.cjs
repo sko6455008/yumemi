7acb4f1c7fe4b5112703bd50139beac493ef82d4
sasaki-kodai <sasaki-kodai@aevic.co.jp>
2023-04-06 22:04:09 +0900

    feat:setting Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json'
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ["react", "@typescript-eslint"],
   extends: [
-    'plugin:react/recommended',
+    "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
-    'airbnb', 
-    'airbnb-typescript',
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
   ],
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
-  }
-}
+  },
+};
